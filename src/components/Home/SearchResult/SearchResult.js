c5cc9babd0d724e07aee3aaad900d7f0d2f28218
RHsanto <rakibulsanto789@gmail.com>
2022-04-26 02:41:59 +0600

    add search field

--- src/components/Home/SearchResult/SearchResult.js
+++ src/components/Home/SearchResult/SearchResult.js
@@ -2,9 +2,9 @@ import React from 'react';
 
 const SearchResult = () => {
   return (
-    <div>
-    <div className="container px-52 mx-auto">
-      <div className="search">
+    <div className='search-section '>
+    <div className="container px-5 ">
+      <div className="search-field">
         <div className="row">
           <div className="col-lg-6 mx-auto">
           <div className="all-category  rounded shadow-lg
@@ -19,18 +19,43 @@ const SearchResult = () => {
      {/* search body */}
      <div className="row">
        <div className="col">
-       <div className="all-inputs bg-white rounded shadow-lg p-4">
+       <div className="all-inputs bg-white rounded shadow-lg p-5">
          <h4>  One Way</h4>
         <div className="d-flex justify-content-center ">
-        <input type="text" name="" id="" />
-        <input type="text" name="" id="" className='ms-2' />
-        <div className="date ms-4">
-          <input type="text" />
-          <input type="text" />
+        <div class="form-floating ">
+           <input type="text" class="form-control"
+               id="floatingInput" placeholder="From"/>
+           <label for="floatingInput">FROM</label>
+       </div>
+       <div class="form-floating ms-2">
+           <input type="text" class="form-control"
+               id="floatingInput" placeholder="To"/>
+           <label for="floatingInput">TO</label>
+       </div>
+        <div className="date ms-4 d-flex">
+        <div class="form-floating ">
+           <input type="text" class="form-control"
+               id="floatingInput" placeholder="name@example.com"/>
+           <label for="floatingInput">JOURNEY DATE</label>
+       </div>
+       <div class="form-floating ">
+           <input type="text" class="form-control"
+               id="floatingInput" placeholder="name@example.com"/>
+           <label for="floatingInput">RETURN DATE</label>
+       </div>
         </div>
-        <input type="text" className='ms-4' />
+        <div class="form-floating ms-4">
+           <input type="text" class="form-control"
+               id="floatingInput" placeholder="name@example.com"/>
+           <label for="floatingInput">TRAVELER CLASS</label>
+       </div>
         </div>
        </div>
+       </div> 
+     </div>
+     <div className="row search-btn">
+       <div className="col-2 mx-auto">
+       <div className="btn btn-warning px-5 py-3">Search</div>
        </div>
      </div>
       </div>
