e36b7ae22e9edcfff8c0e137ee68da47e3c57aeb
RHsanto <rakibulsanto789@gmail.com>
2022-04-26 00:02:24 +0600

    add all components

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 
 import './App.css';
-
+import Home from './components/Home/Home';
+import "slick-carousel/slick/slick.css"; 
+import "slick-carousel/slick/slick-theme.css";
 function App() {
   return (
     <div className="App">
     <h1 class="text-5xl text-blue-500 font-bold underline">
     Hello world
   </h1>
+  <Home/>
     </div>
   );
 }
---
+++ src/components/Home/Home.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Home = () => {
+  return (
+    <h1 class="text-5xl text-blue-500 font-bold underline">
+    Hello world
+  </h1>
+  );
+};
+
+export default Home;
