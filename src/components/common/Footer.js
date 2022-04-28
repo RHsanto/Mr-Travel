08f222c967152b4e13eb11ee8a097b52a0fd9164
RHsanto <rakibulsanto789@gmail.com>
2022-04-29 01:16:37 +0600

    start footer

--- src/components/common/Footer.js
+++ src/components/common/Footer.js
@@ -4,7 +4,67 @@ const Footer = () => {
   return (
     <div>
       <footer>
-        
+        <div className="container-fluid">
+          <div className="row">
+            <div className="col-3">
+              <div className="footer-logo">
+                <h1>Mr. Travel </h1>
+                <p>Lorem ipsum dolor sit 
+                  ametco nsec te tuer adipiscing elitae</p>
+                  <li>mr.travel123@gmail.com</li>
+                  <li>1562 867 5309</li>
+                  <li>Dhaka , Bangladesh</li>
+              </div>
+            </div>
+            <div className="col-3">
+              <h4>Services</h4>
+              <li>About Us</li>
+              <li>How It Works</li>
+              <li>Our Blog</li>
+              <li>Our Services</li>
+              <li>Contact Us</li>
+            </div>
+            <div className="col-3">
+              <h4>Subscribe to our Newsletter</h4>
+              <p>Lorem ipsum dolor sit amet consectetur, 
+                adipisicing elit. Eum, officiis!</p>
+
+                <div className="subs-input">
+                  <input type="text" />
+                  <input type="email" /> <br />
+                 <button>Submit</button>
+                </div>
+            </div>
+            <div className="col-3">
+              <h4>Our Instagram</h4>
+              <div className="gallery-img">
+                <div className="d-flex">
+                <li> <img src="https://travio.smartdemowp.com/wp-content/uploads/2021/02/footer-gallery-1.jpg" 
+                alt="" /></li>
+                <li> <img src="https://travio.smartdemowp.com/wp-content/uploads/2021/02/footer-gallery-1.jpg" 
+                alt="" /></li>
+                <li> <img src="https://travio.smartdemowp.com/wp-content/uploads/2021/02/footer-gallery-1.jpg" 
+                alt="" /></li>
+                <li> <img src="https://travio.smartdemowp.com/wp-content/uploads/2021/02/footer-gallery-1.jpg" 
+                alt="" /></li>
+                </div>
+                <div className="d-flex">
+                <li> <img src="https://travio.smartdemowp.com/wp-content/uploads/2021/02/footer-gallery-1.jpg" 
+                alt="" /></li>
+                <li> <img src="https://travio.smartdemowp.com/wp-content/uploads/2021/02/footer-gallery-1.jpg" 
+                alt="" /></li>
+                <li> <img src="https://travio.smartdemowp.com/wp-content/uploads/2021/02/footer-gallery-1.jpg" 
+                alt="" /></li>
+                <li> <img src="https://travio.smartdemowp.com/wp-content/uploads/2021/02/footer-gallery-1.jpg" 
+                alt="" /></li>
+                </div>
+                
+             </div>
+             
+             
+            </div>
+          </div>
+        </div>
       </footer>
     </div>
   );
