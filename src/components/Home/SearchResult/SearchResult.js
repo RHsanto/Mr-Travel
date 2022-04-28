e10e32f12144a95e1222181a9631dbb4485d1126
RHsanto <rakibulsanto789@gmail.com>
2022-04-29 02:33:35 +0600

    edit search

--- src/components/Home/SearchResult/SearchResult.js
+++ src/components/Home/SearchResult/SearchResult.js
@@ -2,20 +2,29 @@ import React from 'react';
 import { GiCommercialAirplane ,GiPalmTree} from "react-icons/gi";
 import { RiHotelFill } from "react-icons/ri";
 import { FaBus } from "react-icons/fa";
-import {
-  BrowserRouter,
-  Routes,
-  Route,
-  Link
-} from "react-router-dom";
 import Flight from './Flight'
 import Hotel from './Hotel'
 import Tour from './Tour'
 import Bus from './Bus'
+import { useState } from 'react';
 const SearchResult = () => {
+  const[info,setInfo]=useState(<Flight/>)
+
+  const handleFlight =(e)=>{
+ setInfo(<Flight/>)
+  }
+  const handleTour =(e)=>{
+ setInfo(<Tour/>)
+  }
+  const handleHotel =(e)=>{
+ setInfo(<Hotel/>)
+  }
+  const handleBus =(e)=>{
+ setInfo(<Bus/>)
+  }
   return (
   
-    <BrowserRouter>
+ 
       <div className='search-section '>
     <div className="container px-5 ">
       <div className="search-field">
@@ -23,10 +32,10 @@ const SearchResult = () => {
           <div className="col-lg-6 mx-auto col-10">
           <div className="all-category  rounded shadow-lg
        d-flex justify-content-around align-items-center   "> 
-         <li><Link to='/flight'><GiCommercialAirplane/> Flight</Link></li>  
-         <li><Link to='/hotel'><RiHotelFill/> Hotel</Link></li>  
-         <li><Link to='/tour'><GiPalmTree/> Tour</Link></li>  
-         <li><Link to='/bus'><FaBus/> Bus</Link></li>  
+         <li ><button onClick={handleFlight}><GiCommercialAirplane/> Flight</button></li>  
+         <li><button onClick={handleHotel}><RiHotelFill/> Hotel</button></li>  
+         <li><button onClick={handleTour}><GiPalmTree/> Tour</button></li>  
+         <li><button onClick={handleBus}><FaBus/> Bus</button></li>  
          
        </div>
           </div>
@@ -36,13 +45,7 @@ const SearchResult = () => {
        <div className="col">
        <div className="all-inputs bg-white rounded shadow-lg py-5 px-4">
         <h4 className='text-center pt-2 pb-4'>SELECTED YOU DESTINATION</h4>
-         <Routes>
-         <Route path="/" element={<Flight />}/>
-         <Route path="/flight" element={<Flight />}/>
-         <Route path="/hotel" element={<Hotel />}/>
-         <Route path="/tour" element={<Tour />}/>
-         <Route path="/bus" element={<Bus />}/>
-         </Routes>
+         {info}
        </div>
        </div> 
      </div>
@@ -55,7 +58,7 @@ const SearchResult = () => {
     </div>
     </div>
    
-   </BrowserRouter>
+   
   );
 };
 
