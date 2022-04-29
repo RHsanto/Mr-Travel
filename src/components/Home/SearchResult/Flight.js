5bcc3a00811a317bb0e108228d6ab077b1349553
RHsanto <rakibulsanto789@gmail.com>
2022-04-30 03:04:19 +0600

    edit searsch

--- src/components/Home/SearchResult/Flight.js
+++ src/components/Home/SearchResult/Flight.js
@@ -4,35 +4,60 @@ const Flight = () => {
   return (
     <div>
       <div className="d-block  d-md-flex  justify-content-center ">
-       <div className="d-flex">
-       <div class="form-floating ">
+       <div className="d-flex w-100 ">
+       <div class="form-floating w-100">
            <input type="text" class="form-control"
                id="floatingInput" placeholder="From"/>
            <label for="floatingInput">FROM</label>
        </div>
-       <div class="form-floating ms-2">
+       <div class="form-floating ms-2 w-100">
            <input type="text" class="form-control"
                id="floatingInput" placeholder="To"/>
            <label for="floatingInput">TO</label>
        </div>
        </div>
-        <div className="date  d-flex ms-0 ms-lg-3 my-4 my-lg-0">
-        <div class="form-floating ">
-           <input type="text" class="form-control"
+        <div className="d-none d-lg-flex date w-75  ms-0 ms-lg-3 my-4 my-lg-0">
+        <div class="form-floating w-100">
+           <input type='date' class="form-control"
                id="floatingInput" placeholder="name@example.com"/>
            <label for="floatingInput">JOURNEY DATE</label>
        </div>
-       <div class="form-floating ">
-           <input type="text" class="form-control"
+       <div class="form-floating w-100">
+           <input type="date" class="form-control"
                id="floatingInput" placeholder="name@example.com"/>
            <label for="floatingInput">RETURN DATE</label>
        </div>
         </div>
-        <div class="form-floating ms-0  ms-lg-3 ">
-           <input type="text" class="form-control"
+        {/* for mobile */}
+        <div className="d-block d-lg-none date w-100 d-flex ms-0 ms-lg-3 my-4 my-lg-0">
+        <div class="form-floating w-100">
+           <input type='date' class="form-control"
                id="floatingInput" placeholder="name@example.com"/>
-           <label for="floatingInput">TRAVELER CLASS</label>
+           <label for="floatingInput">JOURNEY DATE</label>
        </div>
+       <div class="form-floating w-100">
+           <input type="date" class="form-control"
+               id="floatingInput" placeholder="name@example.com"/>
+           <label for="floatingInput">RETURN DATE</label>
+       </div>
+        </div>
+         <div class="form-floating w-50 ms-2 d-none d-lg-block">
+           <select class="form-select" id="floatingSelect" aria-label="Floating label select example">
+             <option selected>Open this select class</option>
+             <option value="Economic">Economic</option>
+             <option value="Business">Business</option>
+           </select>
+           <label for="floatingSelect">TRAVELER CLASS</label>
+         </div>
+        {/* for mobile */}
+         <div class="form-floating w-100 ms-2 d-bloc d-lg-none">
+           <select class="form-select" id="floatingSelect" aria-label="Floating label select example">
+             <option selected>Open this select class</option>
+             <option value="Economic">Economic</option>
+             <option value="Business">Business</option>
+           </select>
+           <label for="floatingSelect">TRAVELER CLASS</label>
+         </div>
       </div>
     </div>
   );
--- src/components/Home/SearchResult/Hotel.js
+++ src/components/Home/SearchResult/Hotel.js
@@ -17,21 +17,25 @@ const Hotel = () => {
        </div>
         <div className="date ms-0  ms-lg-3 my-3 my-lg-0 d-flex">
         <div class="form-floating ">
-           <input type="text" class="form-control"
+           <input type="date" class="form-control"
                id="floatingInput" placeholder="name@example.com"/>
            <label for="floatingInput">CHECK IN</label>
        </div>
        <div class="form-floating ms-2">
-           <input type="text" class="form-control"
+           <input type="date" class="form-control"
                id="floatingInput" placeholder="name@example.com"/>
            <label for="floatingInput">CHECK OUT</label>
        </div>
         </div>
-        <div class="form-floating ms-0  ms-lg-3">
-           <input type="text" class="form-control"
-               id="floatingInput" placeholder="name@example.com"/>
-           <label for="floatingInput">ROOMS & GUESTS</label>
-       </div>
+ <div class="form-floating w-25  ms-0 ms-lg-3">
+  <select class="form-select" id="floatingSelect" aria-label="Floating label select example">
+    <option  selected>1 Room, 2 Guests</option>
+    <option value="1 Room, 3 Guests">1 Room, 3 Guests</option>
+    <option value="1 Room, 2 Guests">1 Room, 2 Guests</option>
+    <option value="2 Room, 5 Guests">2 Room, 5 Guests</option>
+  </select>
+  <label for="floatingSelect">ROOMS & GUESTS</label>
+    </div>
       </div>
     </div>
   );
