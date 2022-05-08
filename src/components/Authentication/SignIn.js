8fc38cffcb9f58d4154a0010a43db9e523e6fd6c
RHsanto <rakibulsanto789@gmail.com>
2022-05-09 01:17:35 +0600

    add sign in

--- src/components/Authentication/SignIn.js
+++ src/components/Authentication/SignIn.js
@@ -3,7 +3,16 @@ import React from 'react';
 import Gnail from '../../images/gmail.png'
 const SignIn = () => {
   return (
-    <div>Sign</div>
+    <div className='signIn'>
+      <div className="container">
+        <div className="row">
+        <h1 className='text-center mt-5'>Login with your Gmail Account</h1>
+          <div className="text-center my-5">
+           <button className='rounded'> <img src={Gnail}alt="" /> continue with google account</button>
+          </div>
+        </div>
+      </div>
+    </div>
   );
 };
 
