6d30eaeb88b17ffb128e75333d0e9bab5c42dc46
RHsanto <rakibulsanto789@gmail.com>
2022-05-08 18:15:17 +0600

    add tour

--- src/components/AllSearch/TourSearch.js
+++ src/components/AllSearch/TourSearch.js
@@ -1,8 +1,19 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Navbar from '../common/Navbar';
 import ModifyTour from '../ModifySearch/ModifyTour';
+import {RiArrowRightLine} from "react-icons/ri";
+import {MdLocationPin} from "react-icons/md";
+import {HiClock} from "react-icons/hi";
+import {FaUsers} from "react-icons/fa";
 
 const TourSearch = () => {
+  const[tours,setTours]=useState([]);
+
+  useEffect(()=>{
+   fetch('./tour.json')
+   .then(response=>response.json())
+   .then(data=>setTours(data))
+  },[])
   return (
     <div>
       <Navbar/>
@@ -14,10 +25,44 @@ const TourSearch = () => {
       </div>
      </div>
     </div>
-    <div className="common-section">
+    {/* here result */}
+    <div className="common-section ">
     <div className="container">
-     <h1>Tour
-     HotelSearch</h1>
+      <div className="row row-cols-1 row-cols-md-2 g-4">
+        {tours.map(tour=>(
+         <div className="flight mt-5">
+           <div class="card mb-3 ps-0">
+         <div class="row g-0">
+           <div class="col-md-4">
+             <img src={tour?.img} class="img-fluid  h-100 rounded-start" alt="..."/>
+           </div>
+           <div class="col-md-8">
+             <div class="card-body color">
+             <h5 class="card-title fw-bold">{tour?.TourName}</h5>
+              <h6><MdLocationPin/> {tour?.area}, Bangladesh</h6>
+
+              <div className="ms-1">
+              <small ><HiClock/> {tour?.totalTime} hour</small>,
+              <small className='ms-2'><FaUsers/> {tour?.member} people</small> 
+              </div>
+
+              <div className="d-flex justify-content-between align-items-center mt-5">
+              <div className="d-flex">
+              <h6 className='mx-1'><b>BDT {tour?.price}</b></h6>
+               <p><small>(Per Person)</small></p>
+              </div>
+              <div className='select'>
+              <button>booking <RiArrowRightLine className='ms-4 fs-5'/></button> 
+              </div>
+              </div>
+            </div>
+
+           </div>
+         </div>
+       </div>
+         </div>
+        ))}
+      </div>
      </div>
     </div>
     </div>
