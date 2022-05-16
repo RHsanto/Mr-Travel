87a0e1e0e3df42dd6c9fb6930bb283351d443e29
RHsanto <rakibulsanto789@gmail.com>
2022-05-16 14:43:19 +0600

    add accordian

--- src/components/Home/BestOffer/OffersDetails.js
+++ src/components/Home/BestOffer/OffersDetails.js
@@ -1,10 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
-import { RiPlayCircleLine } from "react-icons/ri";
-import { TiUser } from "react-icons/ti";
-import { FcHome } from "react-icons/fc";
-import { MdLanguage } from "react-icons/md";
-import { AiOutlineFieldTime ,AiOutlineStar,AiFillStar} from "react-icons/ai";
+import {AiOutlineStar,AiFillStar} from "react-icons/ai";
 import Navbar from '../../common/Navbar';
 import Rating from 'react-rating';
 import './offer.css'
@@ -23,7 +19,7 @@ const OffersDetails = () => {
     <div>
       <Navbar/>
     <div className="container my-5">
-      <div className="row d-flex  align-items-center">
+      <div className="row mb-3 d-flex  align-items-center">
         <div className="col-lg-6 ">
        <div className="img-box px-3">
          <button className='type-btn '>{offer?.type}</button>
@@ -36,28 +32,46 @@ const OffersDetails = () => {
           </Rating>
          <div className=" mb-3">
            <div >
-           <h3>{offer?.hotelName}</h3>
+           <h3>Review Reservation Details</h3>
            </div>
-           {/* <div className='re-icons  '>
-          
-            </div> */}
          </div>
        {/* <img src={offer?.img} className='border' alt="" /> */}
        <OfferImgSlider/>
-       <p className='mt-4 lh-lg'>{offer?.description}</p></div>
+       <p className='py-4 lh-lg'>{offer?.description}</p>
+       </div>
         </div>
         <div className="col-lg-4">
-         <div className="single-info shadow p-5">
-          <h3><b>${offer?.price}</b></h3>
+         <div className="single-info shadow ">
           <div className='info-icons'>
-            <li><span><FcHome/></span> Instructor : Fahad</li>
-            <li><span><RiPlayCircleLine/></span> Class : {offer?.class} </li>
-            <li><span><TiUser/></span> Students : {offer?.students} </li>
-            <li><span><MdLanguage/></span> Language : English/Bangla</li>
-            <li><span><AiOutlineFieldTime/></span> Duration : 12h 20m 20s</li>
+            <h3>{offer?.hotelName}</h3>
+            <p className='re-icons'><b>ROOM DETAILS</b></p>
+            <li><b>Check in :</b> Friday, May 27, 2022</li>
+            <li><b>Check out :</b> Saturday, May 28, 2022</li>
+            <li><b>Room(s) :</b> 1</li>
+            <li><b>Guest(s) per room :</b> 2</li> 
           </div>
-          <button>Enroll Now</button>
+          <button className='mx-2 mt-4'>Booking Now</button> 
          </div>
+        </div>
+      </div>
+      <div className="row ps-4 pe-3">
+        <div className="col-lg-10  shadow">
+            {/* accordian */}
+            <div class="accordion accordion-flush" id="accordionFlushExample">
+  <div class="accordion-item">
+    <h2 class="accordion-header" id="flush-headingOne">
+      <div class="accordion-button collapsed " type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseOne" aria-expanded="false" aria-controls="flush-collapseOne">
+     <h4 className='fw-bold'> Summary of Charges</h4>
+    
+      </div>
+    </h2>
+    <div id="flush-collapseOne" class="accordion-collapse collapse" aria-labelledby="flush-headingOne" data-bs-parent="#accordionFlushExample">
+      <div class="accordion-body">Placeholder content for this accordion, which is intended to demonstrate the <code>.accordion-flush</code> class. This is the first item's accordion body.</div>
+    </div>
+  </div>
+</div>
+
+          {/* accordian */}
         </div>
       </div>
     </div>
