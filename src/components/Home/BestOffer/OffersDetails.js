6f98c18f6e84ca4d8825228dd2dd39bb496f94a1
RHsanto <rakibulsanto789@gmail.com>
2022-05-16 21:12:34 +0600

    add image slider

--- src/components/Home/BestOffer/OffersDetails.js
+++ src/components/Home/BestOffer/OffersDetails.js
@@ -4,7 +4,7 @@ import {AiOutlineStar,AiFillStar} from "react-icons/ai";
 import Navbar from '../../common/Navbar';
 import Rating from 'react-rating';
 import './offer.css'
-import OfferImgSlider from './OfferImgSlider';
+
 
 const OffersDetails = () => {
   const {id}=useParams();
@@ -35,8 +35,35 @@ const OffersDetails = () => {
            <h3>Review Reservation Details</h3>
            </div>
          </div>
-       {/* <img src={offer?.img} className='border' alt="" /> */}
-       <OfferImgSlider/>
+      {/* sliders */}
+      <div>
+      <div id="carouselExampleControls" class="carousel slide" data-bs-ride="carousel">
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img src={offer?.img} class="d-block w-100" alt="..."/>
+    </div>
+    <div class="carousel-item ">
+      <img src={offer?.slider1} class="d-block w-100" alt="..."/>
+    </div>
+    <div class="carousel-item">
+      <img src={offer?.slider2} class="d-block w-100" alt="..."/>
+    </div>
+    <div class="carousel-item">
+      <img src={offer?.slider3} class="d-block w-100" alt="..."/>
+    </div>
+   
+  </div>
+  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Previous</span>
+  </button>
+  <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Next</span>
+  </button>
+</div>
+    </div>
+      {/* sliders */}
        <p className='py-4 lh-lg'>{offer?.description}</p>
        </div>
         </div>
@@ -47,11 +74,11 @@ const OffersDetails = () => {
             <p className='re-icons'><b>ROOM DETAILS</b></p>
             <li><b>Check in :</b> Friday, May 27, 2022</li>
             <li><b>Check out :</b> Saturday, May 28, 2022</li>
-            <li><b>Room(s) :</b> 1</li>
-            <li><b>Guest(s) per room :</b> 2</li> 
+            <li><b>Room(s) : {offer?.room}</b> </li>
+            <li><b>Guest(s) per room : {offer?.guest}</b> </li> 
           </div>
           <div className="d-flex justify-content-between align-items-center px-2 py-3">
-            <h3>${sum}</h3>
+            <div className='d-flex'><h3>${sum}</h3><small>(per night)</small></div>
           <button className=''>Booking Now</button> 
           </div>
          </div>
