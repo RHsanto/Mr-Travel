bb714a828b213f11a3deb72ee0361db030f7b873
RHsanto <rakibulsanto789@gmail.com>
2022-05-18 02:08:34 +0600

    ad

--- src/components/AllDetails/FlightDetails.js
+++ src/components/AllDetails/FlightDetails.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { useEffect,useState } from 'react';
 import { useParams } from 'react-router-dom';
 import Navbar from '../../components/common/Navbar'
+import './Details.css'
 const FlightDetails = () => {
   const {id}=useParams()
   const [flight,setFlight]=useState([])
@@ -14,8 +15,17 @@ const FlightDetails = () => {
   return (
     <div>
       <Navbar/>
-      <div className="flights">
-      {flight?.planeName}
+      <div className="flight-details">
+      <div className="container">
+        <div className="row">
+         <div className="col-lg-8">
+           <div className="flight-info1 shadow d-flex align-items-center">
+             <img src={flight?.img} className='fd-img' alt="" />
+            <div> <h6>{flight?.planeName}</h6></div>
+           </div>
+         </div>
+        </div>
+      </div>
       </div>
     </div>
   );
