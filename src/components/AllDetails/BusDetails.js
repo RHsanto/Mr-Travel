a610f32fa4ad90bdd41b8c4198e4bc2205052ef9
RHsanto <rakibulsanto789@gmail.com>
2022-06-10 19:39:14 +0600

    [add]

--- src/components/AllDetails/BusDetails.js
+++ src/components/AllDetails/BusDetails.js
@@ -2,9 +2,8 @@ import React from 'react';
 
 const BusDetails = () => {
   return (
-    <div>
-      <h1>BusDetails
-BusDetails</h1>
+    <div className='container'>
+      <h1></h1>
     </div>
   );
 };
