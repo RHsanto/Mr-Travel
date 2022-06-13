f2e12967eb098db0bc04c7dca4f39f467b20d8c9
RHsanto <rakibulsanto789@gmail.com>
2022-06-13 12:21:18 +0600

    add email info

--- src/components/DashBoard/MyBooking.js
+++ src/components/DashBoard/MyBooking.js
@@ -1,7 +1,16 @@
 import React from 'react';
+import { useState } from 'react';
+import useFirebase from '../../hooks/useFirebase';
+
 import Navbar from '../common/Navbar';
 
 const MyBooking = () => {
+  const{user}=useFirebase();
+  const [booking,setBooking]=useState([]);
+  console.log(user?.email);
+  fetch(`https://young-cliffs-75372.herokuapp.com/booking/${user?.email}`)
+  .then(res=>res.json())
+  .then(data=>console.log(data))
   return (
     <div>
       <Navbar/>
