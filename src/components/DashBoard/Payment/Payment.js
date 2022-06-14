0d8c710ec1a0d766fe253183ffa58e1dc505eec8
RHsanto <rakibulsanto789@gmail.com>
2022-06-14 16:41:51 +0600

    add simple payment

--- src/components/DashBoard/Payment/Payment.js
+++ src/components/DashBoard/Payment/Payment.js
@@ -1,12 +1,26 @@
 import React from 'react';
+import { useEffect } from 'react';
+import { useState } from 'react';
+import { useParams } from 'react-router-dom';
+import Navbar from '../../common/Navbar';
 
 
 const Payment = () => {
+  const{id}=useParams()
+  const [payment,setPayment]=useState([])
 
+  useEffect(()=>{
+    fetch(`https://young-cliffs-75372.herokuapp.com/booked/${id}`)
+    .then(res=>res.json())
+    .then(data=>setPayment(data))
+  })
   return (
     <div>
-      <div className="container">
+      <Navbar/>
+      <div className="container my-5">
       <h1>Payment here</h1>
+      <h5>{payment?.price ? <>BDT{payment?.price}</> : <></> }</h5>
+      <h5>{payment?.sum ? <>${payment?.sum}</> : <></> }</h5>
       </div>
     </div>
   );
