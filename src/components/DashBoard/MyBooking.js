cb9f6f249227d45f092ee0a78fb584ffe27e59fb
RHsanto <rakibulsanto789@gmail.com>
2022-06-14 14:12:24 +0600

    add

--- src/components/DashBoard/MyBooking.js
+++ src/components/DashBoard/MyBooking.js
@@ -2,9 +2,6 @@ import React from 'react';
 import { useState } from 'react';
 import useFirebase from '../../hooks/useFirebase';
 import {RiArrowRightLine} from "react-icons/ri";
-import {MdLocationPin} from "react-icons/md";
-import {HiClock} from "react-icons/hi";
-import {FaUsers} from "react-icons/fa";
 import { Link } from 'react-router-dom';
 import Navbar from '../common/Navbar';
 import { useEffect } from 'react';
@@ -26,30 +23,30 @@ const MyBooking = () => {
       <div className="my-5">
     <div className="container">
       <div className="row row-cols-1 row-cols-md-2 g-4">
-        {booking.map(tour=>(
+        {booking.map(data=>(
          <div className="flight mt-5">
            <div className="card mb-3 ps-0">
          <div className="row g-0">
            <div className="col-md-4">
-             <img src={tour?.img} className="img-fluid  h-100 rounded-start" alt="..."/>
+             <img src={data?.img} className="img-fluid  h-100 rounded-start" alt="..."/>
            </div>
            <div className="col-md-8">
              <div className="card-body ">
-             <h5 className="card-title fw-bold">{tour?.TourName}</h5>
-              <h6><MdLocationPin/> {tour?.area}, Bangladesh</h6>
-
-              <div className="ms-1">
-              <small ><HiClock/> {tour?.totalTime} hour</small>,
-              <small className='ms-2'><FaUsers/> {tour?.member} people</small> 
-              </div>
+             <h5 className="card-title fw-bold">{data?.TourName}</h5>
+             <h5 className="card-title fw-bold">{data?.hotelName}</h5>
+             <div className='d-flex gap-5 align-items-center'>
+             <h6 className="card-title fw-bold">{data?.from}</h6>
+             <h6 className="card-title fw-bold">{data?.to}</h6>
+             </div>
+             <h6>Types: {data?.types}</h6>
 
               <div className="d-flex justify-content-between align-items-center mt-5">
               <div className="d-flex">
-              <h6 className='mx-1'><b>BDT {tour?.price}</b></h6>
+              <h6 className='mx-1'><b>BDT {data?.price}</b></h6>
                <p><small>(Per Person)</small></p>
               </div>
               <div className='select'>
-              <Link to={`/tours/${tour._id}`}>
+              <Link to={`/tours/${data._id}`}>
               <button>Booking <RiArrowRightLine className='ms-4 fs-5'/></button> 
               </Link>
               </div>
