8c8b682d745637ad8c49fd79895b65394223811f
RHsanto <rakibulsanto789@gmail.com>
2022-06-15 23:47:03 +0600

    add

--- src/components/DashBoard/MyBooking.js
+++ src/components/DashBoard/MyBooking.js
@@ -27,42 +27,48 @@ const MyBooking = () => {
       <div className="my-5">
     <div className="container">
       <div className="row row-cols-1 row-cols-md-2 g-4">
+        {booking?.length===0 ? <>Empty</> :
+        
+       <>
         {booking.map(data=>(
-         <div className="flight mt-5" key={data._id}>
-           <div className="card mb-3 ps-0">
-         <div className="row g-0">
-           <div className="col-md-4">
-             <img src={data?.img} className="img-fluid  h-100 rounded-start" alt="..."/>
-           </div>
-           <div className="col-md-8">
-             <div className="card-body ">
-             <div>
-             {data?.from  ? <div className='d-flex  align-items-center'> 
-             <h6 className="card-title fw-bold">{data?.from}</h6>
-             <h6  className='arrow mx-5'><AiOutlineSwap/></h6>
-             <h6 className="card-title fw-bold">{data?.to}</h6></div> : <>
-             <h5 className="card-title fw-bold">{data?.TourName}</h5>
-             <h5 className="card-title fw-bold">{data?.hotelName}</h5></>
-              }
-             </div>
-             <h6>Types: {data?.types}</h6>
-
-              <div className="d-flex justify-content-between align-items-center mt-5">
-              <div className="d-flex">
-              <h6 className='mx-1'><b> {data?.price} {data?.sum}</b></h6>
-               <p><small>(Per Person)</small></p>
-              </div>
-              <div className='select-btn gap-2 d-flex'>
-              <Link to={`/payment/${data?._id}`}> <button className='btn btn-success '><SiFampay/> Payment</button></Link>
-               <button className='btn btn-danger'><MdDelete/> Delete</button>
-              </div>
-              </div>
+          <div className="flight mt-5" key={data._id}>
+            <div className="card mb-3 ps-0">
+          <div className="row g-0">
+            <div className="col-md-4">
+              <img src={data?.img} className="img-fluid  h-100 rounded-start" alt="..."/>
             </div>
-           </div>
-         </div>
-       </div>
-         </div>
-        ))}
+            <div className="col-md-8">
+              <div className="card-body ">
+              <div>
+              {data?.from  ? <div className='d-flex  align-items-center'> 
+              <h6 className="card-title fw-bold">{data?.from}</h6>
+              <h6  className='arrow mx-5'><AiOutlineSwap/></h6>
+              <h6 className="card-title fw-bold">{data?.to}</h6></div> : <>
+              <h5 className="card-title fw-bold">{data?.TourName}</h5>
+              <h5 className="card-title fw-bold">{data?.hotelName}</h5></>
+               }
+              </div>
+              <h6>Types: {data?.types}</h6>
+ 
+               <div className="d-flex justify-content-between align-items-center mt-5">
+               <div className="d-flex">
+               <h6 className='mx-1'><b> {data?.price} {data?.sum}</b></h6>
+                <p><small>(Per Person)</small></p>
+               </div>
+               <div className='select-btn gap-2 d-flex'>
+               <Link to={`/payment/${data?._id}`}> <button className='btn btn-success '><SiFampay/> Payment</button></Link>
+                <button className='btn btn-danger'><MdDelete/> Delete</button>
+               </div>
+               </div>
+             </div>
+            </div>
+          </div>
+        </div>
+          </div>
+         ))}</>
+        
+        }
+        
       </div>
      </div>
     </div>
