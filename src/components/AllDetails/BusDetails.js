2db5bb5321af9425af2705c55cf4a325a382e1cf
RHsanto <rakibulsanto789@gmail.com>
2022-10-27 19:28:28 +0600

    bus

--- src/components/AllDetails/BusDetails.js
+++ src/components/AllDetails/BusDetails.js
@@ -2,8 +2,8 @@ import React from 'react';
 
 const BusDetails = () => {
   return (
-    <div className='container'>
-      <h1>Here Show all Details info</h1>
+    <div className='container my-5'>
+      <h1 className='text-center'>Here Show all Details info</h1>
     </div>
   );
 };
