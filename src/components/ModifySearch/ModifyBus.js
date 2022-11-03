6d9c25cce88937e2ebd1044efe03185ed6c9d34a
RHsanto <rakibulsanto789@gmail.com>
2022-11-03 22:08:05 +0600

    change api

--- src/components/ModifySearch/ModifyBus.js
+++ src/components/ModifySearch/ModifyBus.js
@@ -1,19 +1,20 @@
-import React, { useState } from 'react';
-import { useEffect } from 'react';
-import BusResults from './ModifyResult/BusResults';
+import React, { useState } from "react";
+import { useEffect } from "react";
+import BusResults from "./ModifyResult/BusResults";
 
 const ModifyBus = () => {
   const [from, setFrom] = useState([]);
   const [searchTo, setSearchTo] = useState([]);
   const [updatedValues, setUpdatedValues] = useState();
-  const[results,setResults]=useState([]);
+  const [results, setResults] = useState([]);
 
   useEffect(() => {
     fetch("http://localhost:8000/busInfo")
       .then(res => res.json())
       .then(data => {
-        setResults(data)
-        setFrom(data)});
+        setResults(data);
+        setFrom(data);
+      });
   }, []);
 
   const handleFrom = e => {
@@ -22,65 +23,79 @@ const ModifyBus = () => {
     const newUpdatedValues = { ...updatedValues };
     newUpdatedValues[name] = value;
     setUpdatedValues(newUpdatedValues);
- 
   };
   console.log(updatedValues);
 
-  const handleResults =(e)=>{
-    const searchResults = results.filter((items)=>{
-      if( updatedValues?.from || updatedValues?.to  === 
-         items?.from || items?.to ){
-        return (items)
-  
+  const handleResults = e => {
+    const searchResults = results.filter(items => {
+      if (updatedValues?.from || updatedValues?.to === items?.from || items?.to) {
+        return items;
       }
-    })
-    setSearchTo(searchResults)
-  }
-
-
-
+    });
+    setSearchTo(searchResults);
+  };
 
   return (
-    <div className='pb-5'>
-    <div className='d-block  d-md-flex  gap-3'>
-  <div className="d-flex  w-100">
-  <div className="form-floating w-50 ms-2 ">
+    <div className="pb-5">
+      <div className="d-block  d-md-flex  gap-3">
+        <div className="d-flex  w-100">
+          <div className="form-floating w-50 ms-2 ">
+            <select
+              onChange={handleFrom}
+              className="form-select"
+              id="floatingSelect"
+              aria-label="Floating label select example"
+            >
+              <option value="">--Select From Destination--</option>
+              {from.map(tours => (
+                <option value={tours?.from}>{tours?.from}</option>
+              ))}
+            </select>
 
-<select onChange={handleFrom} className="form-select" id="floatingSelect" aria-label="Floating label select example">
-<option value="">--Select From Destination--</option>
-          {from.map(tours => (
-            <option value={tours?.from}>{tours?.from}</option>
-          ))}
-</select>
+            <label for="floatingSelect">FROM</label>
+          </div>
+          <div className="form-floating w-50 ms-2 ">
+            <select
+              onChange={handleFrom}
+              className="form-select"
+              id="floatingSelect"
+              aria-label="Floating label select example"
+            >
+              <option value="">--Select From Destination--</option>
+              {from.map(tours => (
+                <option value={tours?.to}>{tours?.to}</option>
+              ))}
+            </select>
+            <label for="floatingSelect">TO</label>
+          </div>
+        </div>
+        <div className="form-floating  mt-3 mt-md-0 w-50 d-none d-md-block">
+          <input
+            onChange={handleFrom}
+            type="date"
+            className="form-control"
+            id="floatingInput"
+            placeholder="name@example.com"
+          />
+          <label for="floatingInput">JOURNEY DATE</label>
+        </div>
+        {/* for mobile */}
+        <div className="form-floating  mt-3 mt-md-0  d-block d-md-none">
+          <input
+            type="date"
+            className="form-control"
+            id="floatingInput"
+            placeholder="name@example.com"
+          />
+          <label for="floatingInput">JOURNEY DATE</label>
+        </div>
 
-<label for="floatingSelect">FROM</label>
-  </div> 
-  <div className="form-floating w-50 ms-2 ">
-<select onChange={handleFrom} className="form-select" id="floatingSelect" aria-label="Floating label select example">
-<option value="">--Select From Destination--</option>
-          {from.map(tours => (
-            <option value={tours?.to}>{tours?.to}</option>
-          ))}
-</select>
-<label for="floatingSelect">TO</label>
-  </div>
-  </div>
-    <div className="form-floating  mt-3 mt-md-0 w-50 d-none d-md-block">
-         <input onChange={handleFrom} type="date" className="form-control"
-             id="floatingInput" placeholder="name@example.com"/>
-         <label for="floatingInput">JOURNEY DATE</label>
-     </div>
-     {/* for mobile */}
-    <div className="form-floating  mt-3 mt-md-0  d-block d-md-none">
-         <input type="date" className="form-control"
-             id="floatingInput" placeholder="name@example.com"/>
-         <label for="floatingInput">JOURNEY DATE</label>
-     </div>
-
-  <button onClick={handleResults} className='modify-btn mt-md-0 mt-3 py-3 py-lg-0'>Modify Search</button>
-  </div>
-  <BusResults searchValue={searchTo} />
- </div>
+        <button onClick={handleResults} className="modify-btn mt-md-0 mt-3 py-3 py-lg-0">
+          Modify Search
+        </button>
+      </div>
+      <BusResults searchValue={searchTo} />
+    </div>
   );
 };
 
