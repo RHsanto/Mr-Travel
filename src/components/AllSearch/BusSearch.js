21f90a81df105ecb705cb05ed4c142b5aa445b55
RHsanto <rakibulsanto789@gmail.com>
2022-11-08 00:00:32 +0600

    set modify dataa

--- src/components/AllSearch/BusSearch.js
+++ src/components/AllSearch/BusSearch.js
@@ -1,5 +1,3 @@
-/* eslint-disable react-hooks/exhaustive-deps */
-/* eslint-disable array-callback-return */
 /* eslint-disable eqeqeq */
 
 import React from "react";
@@ -16,32 +14,50 @@ const BusSearch = () => {
   const results = localStorage.getItem("busData");
   const [busData, setBusData] = useState([]);
   const[searchResults,setSearResult] =useState([])
-
+  const [updatedValues, setUpdatedValues] = useState();
   // here data parsed
   let parsed 
   if(results != undefined)
   {parsed = JSON.parse(results);}
-  // console.log( parsed?.from);
+  console.log(parsed);
 
   // here fetch api
   useEffect(() => {
     fetch(" http://localhost:8000/busInfo")
       .then(res => res.json())
-      .then(data => {
-        setBusData(data)
-        ;});
+      .then(data => setBusData(data));
   }, []);
 
+  const handleFrom = e => {
+    const name = e.target.name;
+    const value = e.target.value;
+    const newUpdatedValues = { ...updatedValues };
+    newUpdatedValues[name] = value;
+    setUpdatedValues(newUpdatedValues);
+    localStorage.setItem('busData', JSON.stringify(newUpdatedValues))
+  };
+  // console.log(updatedValues);
 // here get data and fetch data filter
 useEffect(()=>{
   const searchResults = busData.filter((items)=>{
-    if( parsed?.from   ===  items?.from ){
+    if( parsed?.from ===  items?.from ){
       return (items)
     }
-    // console.log(items);
   })
   setSearResult(searchResults)
 },[busData])
+
+
+
+const handleResults = e => {
+  const searchResults = busData.filter(items => {
+    if (updatedValues?.from  === items?.from ) {
+      return items;
+    }
+  });
+  setSearResult(searchResults);
+  // console.log(searchResults);
+};
 
   return (
     <div>
@@ -54,7 +70,67 @@ useEffect(()=>{
           </div>
         </div>
       </div> */}
-   {/* here result */}
+       <div className="pb-5">
+      <div className="d-block  d-md-flex  gap-3">
+        <div className="d-flex  w-100">
+          <div className="form-floating w-50 ms-2 ">
+            <select
+            name="from"
+              onChange={handleFrom}
+              className="form-select"
+              id="floatingSelect"
+              aria-label="Floating label select example"
+            >
+              <option value="">--Select From Destination--</option>
+              {busData.map(tours => (
+                <option value={tours?.from}>{tours?.from}</option>
+              ))}
+            </select>
+            <label for="floatingSelect">FROM</label>
+          </div>
+          <div className="form-floating w-50 ms-2 ">
+            <select
+            name="to"
+              onChange={handleFrom}
+              className="form-select"
+              id="floatingSelect"
+              aria-label="Floating label select example"
+            >
+              <option value="">--Select From Destination--</option>
+              {busData.map(tours => (
+                <option value={tours?.to}>{tours?.to}</option>
+              ))}
+            </select>
+            <label for="floatingSelect">TO</label>
+          </div>
+        </div>
+        <div className="form-floating  mt-3 mt-md-0 w-50 d-none d-md-block">
+          <input
+          name="date"
+            onChange={handleFrom}
+            type="date"
+            className="form-control"
+            id="floatingInput"
+            placeholder="name@example.com"
+          />
+          <label for="floatingInput">JOURNEY DATE</label>
+        </div>
+        {/* for mobile */}
+        <div className="form-floating  mt-3 mt-md-0  d-block d-md-none">
+          <input
+            type="date"
+            className="form-control"
+            id="floatingInput"
+            placeholder="name@example.com"
+          />
+          <label for="floatingInput">JOURNEY DATE</label>
+        </div>
+
+        <button onClick={handleResults} className="modify-btn mt-md-0 mt-3 py-3py-lg-0">
+          Modify Search
+        </button>
+      </div>
+      {/* here all results */}
       <div className="common-section ">
         <div className="container">
            <div className="row row-cols-1 row-cols-md-2 g-4">
@@ -114,6 +190,9 @@ useEffect(()=>{
           </div> 
         </div>
       </div>
+       </div>
+   {/* here result */}
+     
     </div>
   );
 };
