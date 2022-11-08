7dbe6ce4414c49e2a378000912cafc133f2725bf
RHsanto <rakibulsanto789@gmail.com>
2022-11-09 00:25:04 +0600

    set design

--- src/components/Home/SearchResult/Tour.js
+++ src/components/Home/SearchResult/Tour.js
@@ -1,16 +1,43 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 
 const Tour = () => {
+  const [updatedValues, setUpdatedValues] = useState();
+  const [tourInfo, setTourInfo] = useState([]);
+
+  // here fetch data
+  useEffect(() => {
+    fetch(" http://localhost:8000/tourInfo")
+      .then(res => res.json())
+      .then(data => setTourInfo(data));
+  }, []);
+
+  // get input items when selected
+  const handleFrom = e => {
+    const name = e.target.name;
+    const value = e.target.value;
+    const newUpdatedValues = { ...updatedValues };
+    newUpdatedValues[name] = value;
+    setUpdatedValues(newUpdatedValues);
+    localStorage.setItem("tourData", JSON.stringify(newUpdatedValues));
+  };
   return (
     <div>
       <div className="form-floating w-100">
-        <input
-          type="text"
-          className="form-control"
-          id="floatingInput"
-          placeholder="name@example.com"
-        />
+        <select
+          onChange={handleFrom}
+          name="location"
+          className="form-select"
+          id="floatingSelect"
+          aria-label="Floating label select example"
+        >
+          <option>--Select Tour Destination--</option>
+          {tourInfo.map(tourData => (
+            <option key={tourData?.id} value={tourData?.TourName}>
+              {tourData?.TourName}
+            </option>
+          ))}
+        </select>
         <label for="floatingInput">LOCATION / TOUR</label>
       </div>
       <div className="search-btn">
