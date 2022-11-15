09d7bfbf7a979bfcf3053e761b20d218c4769944
RHsanto <rakibulsanto789@gmail.com>
2022-11-15 19:26:33 +0600

    add reveiws scetion

--- src/components/Home/Reviews/Reviews.js
+++ src/components/Home/Reviews/Reviews.js
@@ -45,18 +45,12 @@ const Reviews = () => {
   return (
     <div className='review-section '>
       <div className="reviews">
+      <h1 className='text-center'>Reviews</h1>
       <div className="container ">
-       {/* <div className="row mb-5">
-         <div className="col-lg-6 mx-auto text-center text-light ">
-         <h1 className='review-title fw-bold'> TO REVIEWS</h1>
-         <p className='review-info mx-3'>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vero quas ad dolor consequatur repudiandae nostrum accusamus facere quod
-        iusto laudantium obcaecati qui molestias, sequi saepe.</p>
-         </div>
-       </div> */}
        <div className="row sliders">
        <Slider  {...settings}>
          <div className="col-lg-4">
-           <div className=" review-card bg-light p-3 
+           <div className="card bg-light p-3 
            align-items-center rounded-3 d-block d-lg-flex gap-4">
             <div className="review-img">
               <img src="https://setsail.qodeinteractive.com/wp-content/uploads/2018/09/user-img-5.png" alt="" />
