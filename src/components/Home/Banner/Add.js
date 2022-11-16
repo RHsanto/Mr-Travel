701c047be8744e5bf9e6474431b85942ba1bcd35
RHsanto <rakibulsanto789@gmail.com>
2022-11-16 20:11:13 +0600

    add reveiws scetion

--- src/components/Home/Banner/Add.js
+++ src/components/Home/Banner/Add.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React from "react";
 // import Add1 from '../../../images/add1.png'
 // import Add2 from '../../../images/add2.png'
 // import Add3 from '../../../images/add3.png'
@@ -7,30 +7,37 @@ import { FaStar, FaStarHalfAlt } from "react-icons/fa";
 
 const Add = () => {
   return (
-    <div className='reviews-section'>
-     <div className="container">
-       <div className="row">
-        <div className="col-lg-4 ">
-        <div className="review-item">
-          <div className="reviewer">
-           <img src="https://setsail.qodeinteractive.com/wp-content/uploads/2018/09/user-img-5.png" alt="" />
-          </div>
-          <div className=" d-flex justify-content-center ratings">
-              <FaStar/>
-              <FaStar/>
-              <FaStar/>
-              <FaStar/>
-              <FaStarHalfAlt/>
-             </div> 
-          <div className="testi-text p-4 text-center">
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel maxime amet mollitia nulla, fugit eum magni? Esse, culpa ex. Animi, ut quod! Unde vel repudiandae esse velit architecto quos quaerat!</p>
-             <h5 className='clients '>Husaain jaif</h5>
-             <small>Travelers</small>
+    <div className="reviews-section">
+      <div className="container">
+        <div className="row">
+          <div className="col-lg-4 ">
+            <div className="review-item">
+              <div className="reviewer">
+                <img
+                  src="https://setsail.qodeinteractive.com/wp-content/uploads/2018/09/user-img-5.png"
+                  alt=""
+                />
+              </div>
+              <div className=" d-flex justify-content-center ratings">
+                <FaStar />
+                <FaStar />
+                <FaStar />
+                <FaStar />
+                <FaStarHalfAlt />
+              </div>
+              <div className="review-text p-4 text-center">
+                <p>
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel maxime amet mollitia
+                  nulla, fugit eum magni? Esse, culpa ex. Animi, ut quod! Unde vel repudiandae esse
+                  velit architecto quos quaerat!
+                </p>
+                <h5 className="clients">Husaain jaif</h5>
+                <small>Travelers</small>
+              </div>
+            </div>
           </div>
         </div>
-        </div>
-       </div>
-     </div>
+      </div>
     </div>
   );
 };
