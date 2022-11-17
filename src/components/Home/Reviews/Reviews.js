1bed31f884fdce2c6b8b5365437a4c987c62e225
RHsanto <rakibulsanto789@gmail.com>
2022-11-17 20:33:54 +0600

    add reveiws scetion

--- src/components/Home/Reviews/Reviews.js
+++ src/components/Home/Reviews/Reviews.js
@@ -1,17 +1,16 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState } from "react";
 import { FaStar, FaStarHalfAlt } from "react-icons/fa";
 import Slider from "react-slick";
-import './Reviews.css'
+import "./Reviews.css";
 const Reviews = () => {
+  const [reviews, setReviews] = useState([]);
 
-  const [reviews,setReviews] =useState([])
-
-  useEffect(()=>{
-   fetch("https://mr-travel-server.onrender.com/reviews")
-   .then(res=>res.json())
-   .then(data=>setReviews(data))
-
-  },[])
+  useEffect(() => {
+    fetch("https://mr-travel-server.onrender.com/reviews")
+      .then(res => res.json())
+      .then(data => setReviews(data));
+  }, []);
+  // here slider settings
   var settings = {
     infinite: true,
     speed: 3000,
@@ -30,7 +29,7 @@ const Reviews = () => {
           infinite: true,
           autoplay: true,
           autoplaySpeed: 2000,
-        }
+        },
       },
       {
         breakpoint: 600,
@@ -39,8 +38,8 @@ const Reviews = () => {
           slidesToScroll: 1,
           initialSlide: 2,
           autoplaySpeed: 2000,
-          autoplay: true
-        }
+          autoplay: true,
+        },
       },
       {
         breakpoint: 480,
@@ -48,46 +47,40 @@ const Reviews = () => {
           slidesToShow: 1,
           slidesToScroll: 1,
           autoplaySpeed: 2000,
-        }
-      }
-    ]
+        },
+      },
+    ],
   };
   return (
-    <div className='review-section'>
-      <h1 className='text-center title mb-5'> Traveler Experience</h1>
+    <div className="review-section">
+      <h1 className="text-center title mb-5"> Traveler Experience</h1>
       <div className="container">
-       <div className="row ">
-       <Slider  {...settings}>
-        {reviews.map(data=>
-        <div key={data?._id} className="col-lg-4 ">
-        <div className=" review-item m-4 ">
-          <div className="reviewer">
-            <img
-              src={data?.client_image}
-              alt="img"
-            />
-          </div>
-          <div className=" d-flex justify-content-center ratings">
-            <FaStar />
-            <FaStar />
-            <FaStar />
-            <FaStar />
-            <FaStarHalfAlt />
-          </div>
-          <div className="review-text p-4 text-center">
-            <p> {data?.description}</p>
-            <h5 className="clients">{data?.client}</h5>
-            <small>Travelers</small>
-          </div>
+        <div className="row ">
+          <Slider {...settings}>
+            {reviews.map(data => (
+              <div key={data?._id} className="col-lg-4 ">
+                <div className=" review-item m-4 ">
+                  <div className="reviewer">
+                    <img src={data?.client_image} alt="img" />
+                  </div>
+                  <div className=" d-flex justify-content-center ratings">
+                    <FaStar />
+                    <FaStar />
+                    <FaStar />
+                    <FaStar />
+                    <FaStarHalfAlt />
+                  </div>
+                  <div className="review-text p-4 text-center">
+                    <p> {data?.description}</p>
+                    <h5 className="clients">{data?.client}</h5>
+                    <small>Travelers</small>
+                  </div>
+                </div>
+              </div>
+            ))}
+          </Slider>
         </div>
       </div>
-          )}
-         
-       
-        
-        </Slider>
-       </div>
-     </div> 
     </div>
   );
 };
