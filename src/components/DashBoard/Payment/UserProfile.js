c7d86d91682a4c18164c9f8b172753a0623d7f3e
RHsanto <rakibulsanto789@gmail.com>
2022-11-18 22:42:34 +0600

    add dashboard

--- src/components/DashBoard/Payment/UserProfile.js
+++ src/components/DashBoard/Payment/UserProfile.js
@@ -5,7 +5,25 @@ const UserProfile = () => {
   return (
     <div>
       <Navbar/>
-      <h1>UserProfile</h1>
+     <div className="user-profile-section my-5">
+      <div className="container">
+        <div className="row d-flex justify-content-center">
+          <div className="col-lg-8 ">
+            <div className="card">
+              <h3 className='text-center profile-title'>User Profile</h3>
+             <div className="p-4">
+             <p className='mb-5'>Personal Information :</p>
+              <div className="user-image-item">
+                <div className="user-img">
+                  <img  src="https://gozayaan.sgp1.digitaloceanspaces.com/media/profile_picture/user_587885df-462a-4ebf-8753-61be1498b64e/re-1.png" alt="" />
+                </div>
+              </div>
+             </div>
+            </div>
+          </div>
+        </div>
+      </div>
+     </div>
     </div>
   );
 };
