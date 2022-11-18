30898b0ab35aa4db21fb46028f5a7633cceda142
RHsanto <rakibulsanto789@gmail.com>
2022-11-18 21:44:13 +0600

    add dashboard

--- src/components/DashBoard/MyBooking.js
+++ src/components/DashBoard/MyBooking.js
@@ -27,73 +27,79 @@ const MyBooking = () => {
         <div className="my-5">
           <div className="container ">
             <div>
-              {booking?.length===0 ? <div className='text-center'>
-                  <h3 className='mb-5'>Ooops. It's Empty </h3>
-                <img className='' src="https://ouch-cdn2.icons8.com/8ly2UKxjmBZ-vzfWTtRw0InBYa56IV277LHx1cSi5kI/rs:fit:256:256/czM6Ly9pY29uczgu/b3VjaC1wcm9kLmFz/c2V0cy9zdmcvNTEw/L2ExNGNhNjM0LWMy/NTMtNDExMC05MmU5/LTY5Mzk3YzJkNzE4/Yy5zdmc.png" alt="" />
-            </div> :
-        
-              <div className="row row-cols-1 row-cols-md-2 g-4">
-                {booking.map(data => (
-                  <div className="flight mt-3" key={data._id}>
-                    <div className="card mb-3 ps-0">
-                      <div className="row g-0">
-                        <div className="col-md-4">
-                          <img
-                            src={data?.img}
-                            className="img-fluid  h-100 rounded-start"
-                            alt="..."
-                          />
-                        </div>
-                        <div className="col-md-8">
-                          <div className="card-body ">
-                            <div>
-                              {data?.from ? (
-                                <div className="d-flex  align-items-center">
-                                  <h6 className="card-title fw-bold">{data?.from}</h6>
-                                  <h6 className="arrow mx-5">
-                                    <AiOutlineSwap />
-                                  </h6>
-                                  <h6 className="card-title fw-bold">{data?.to}</h6>
-                                </div>
-                              ) : (
-                                <>
-                                  <h5 className="card-title fw-bold">{data?.TourName}</h5>
-                                  <h5 className="card-title fw-bold">{data?.hotelName}</h5>
-                                </>
-                              )}
-                            </div>
-                            <h6>booking of {data?.types}</h6>
-
-                            <div className="d-flex justify-content-between align-items-center mt-5">
-                              <div className="d-flex">
-                                <h6 className="mx-1">
-                                  <b>
-                                    {" "}
-                                    {data?.price} {data?.sum}
-                                  </b>
-                                </h6>
-                                <p>
-                                  <small>(Per Person)</small>
-                                </p>
+              {booking?.length === 0 ? (
+                <div className="text-center">
+                  <h3 className="mb-5">Ooops. It's Empty </h3>
+                  <img
+                    className=""
+                    src="https://ouch-cdn2.icons8.com/8ly2UKxjmBZ-vzfWTtRw0InBYa56IV277LHx1cSi5kI/rs:fit:256:256/czM6Ly9pY29uczgu/b3VjaC1wcm9kLmFz/c2V0cy9zdmcvNTEw/L2ExNGNhNjM0LWMy/NTMtNDExMC05MmU5/LTY5Mzk3YzJkNzE4/Yy5zdmc.png"
+                    alt=""
+                  />
+                </div>
+              ) : (
+                <div className="row row-cols-1 row-cols-md-2 g-4">
+                  {booking.map(data => (
+                    <div className="flight mt-3" key={data._id}>
+                      <div className="card mb-3 ps-0">
+                        <div className="row g-0">
+                          <div className="col-md-4">
+                            <img
+                              src={data?.img}
+                              className="img-fluid  h-100 rounded-start"
+                              alt="..."
+                            />
+                          </div>
+                          <div className="col-md-8">
+                            <div className="card-body ">
+                              <div>
+                                {data?.from ? (
+                                  <div className="d-flex  align-items-center">
+                                    <h6 className="card-title fw-bold">{data?.from}</h6>
+                                    <h6 className="arrow mx-5">
+                                      <AiOutlineSwap />
+                                    </h6>
+                                    <h6 className="card-title fw-bold">{data?.to}</h6>
+                                  </div>
+                                ) : (
+                                  <>
+                                    <h5 className="card-title fw-bold">{data?.TourName}</h5>
+                                    <h5 className="card-title fw-bold">{data?.hotelName}</h5>
+                                  </>
+                                )}
                               </div>
-                              <div className="select-btn gap-2 d-flex">
-                                <Link to={`/payment/${data?._id}`}>
-                                  <button className="btn payment-btn">
-                                    <SiFampay /> Payment
+                              <h6>booking of {data?.types}</h6>
+
+                              <div className="d-flex justify-content-between align-items-center mt-5">
+                                <div className="d-flex">
+                                  <h6 className="mx-1">
+                                    <b>
+                                      {" "}
+                                      {data?.price} {data?.sum}
+                                    </b>
+                                  </h6>
+                                  <p>
+                                    <small>(Per Person)</small>
+                                  </p>
+                                </div>
+                                <div className="select-btn gap-2 d-flex">
+                                  <Link to={`/payment/${data?._id}`}>
+                                    <button className="btn payment-btn">
+                                      <SiFampay /> Payment
+                                    </button>
+                                  </Link>
+                                  <button className="btn btn-danger">
+                                    <MdDelete /> Delete
                                   </button>
-                                </Link>
-                                <button className="btn btn-danger">
-                                  <MdDelete /> Delete
-                                </button>
+                                </div>
                               </div>
                             </div>
                           </div>
                         </div>
                       </div>
                     </div>
-                  </div>
-                ))}
-              </div>}
+                  ))}
+                </div>
+              )}
             </div>
           </div>
         </div>
