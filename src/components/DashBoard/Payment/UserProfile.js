0c6149511473a235e947aaf8821f9633e3e17ddf
RHsanto <rakibulsanto789@gmail.com>
2022-11-19 15:53:39 +0600

    add dashboard

--- src/components/DashBoard/Payment/UserProfile.js
+++ src/components/DashBoard/Payment/UserProfile.js
@@ -13,7 +13,7 @@ const UserProfile = () => {
                 <h3 className="text-center profile-title">User Profile</h3>
                 <div className="p-4 card-item">
                   <h5 className="mb-4">Personal Information :</h5>
-                  <div className="user-image-item  d-flex justify-content-between ">
+                  <div className="user-image-item mb-5 d-flex justify-content-between ">
                     <div className="user-img ">
                       <img
                         src="https://gozayaan.sgp1.digitaloceanspaces.com/media/profile_picture/user_587885df-462a-4ebf-8753-61be1498b64e/re-1.png"
@@ -27,125 +27,142 @@ const UserProfile = () => {
                     </div>
                   </div>
                   {/* form */}
-                <div className="d-flex">
-                <div className="w-50">
-                  <div class="mb-3 ">
-                    <label for="exampleFormControlInput1" class="form-label">
-                    Your Email 
-                    </label>
-                    <input
-                      type="email"
-                      class="form-control"
-                      id="exampleFormControlInput1"
-                      placeholder="Your Email :"
-                    />
-                  </div>
-                  <div class="mb-3  ">
-                    <label for="exampleFormControlInput1" class="form-label">
-                    Your Email 
-                    </label>
-                    <input
-                      type="email"
-                      class="form-control"
-                      id="exampleFormControlInput1"
-                      placeholder="Your Email :"
-                    />
-                  </div>
-                  <div class="mb-3 ">
-                    <label for="exampleFormControlInput1" class="form-label">
-                    Your Email 
-                    </label>
-                    <input
-                      type="email"
-                      class="form-control"
-                      id="exampleFormControlInput1"
-                      placeholder="Your Email :"
-                    />
-                  </div>
-                  <div class="mb-3  ">
-                    <label for="exampleFormControlInput1" class="form-label">
-                    Your Email 
-                    </label>
-                    <input
-                      type="email"
-                      class="form-control"
-                      id="exampleFormControlInput1"
-                      placeholder="Your Email :"
-                    />
-                  </div>
-                  <div class="mb-3  ">
-                    <label for="exampleFormControlInput1" class="form-label">
-                    Your Email 
-                    </label>
-                    <input
-                      type="email"
-                      class="form-control"
-                      id="exampleFormControlInput1"
-                      placeholder="Your Email :"
-                    />
-                  </div>
+                  {/*Basic Info  */}
+                  <h5 className="mb-5 fw-bold">Basic Info :</h5>
+                  <div className="d-flex">
+                    <div className="from-one w-50">
+                      <div class="mb-4 ">
+                        <label for="exampleFormControlInput1" class="form-label">
+                          First Name
+                        </label>
+                        <input
+                          type="text"
+                          class="form-control"
+                          id="exampleFormControlInput1"
+                          placeholder="Your Email :"
+                        />
+                      </div>
+                      <div class="mb-4 ">
+                        <label for="exampleFormControlInput2" class="form-label">
+                          Your email
+                        </label>
+                        <input
+                          type="email"
+                          class="form-control"
+                          id="exampleFormControlInput2"
+                          placeholder="Your Email :"
+                        />
+                      </div>
+                      <div class="mb-4 ">
+                        <label for="exampleFormControlInput2" class="form-label">
+                          National ID
+                        </label>
+                        <input
+                          type="number"
+                          class="form-control"
+                          id="exampleFormControlInput2"
+                          placeholder="Your Email :"
+                        />
+                      </div>
+                    </div>
+                    <div className="from-two w-50 ms-4">
+                      <div class="mb-4 ">
+                        <label for="exampleFormControlInput4" class="form-label">
+                          Last Name
+                        </label>
+                        <input
+                          type="text"
+                          class="form-control"
+                          id="exampleFormControlInput4"
+                          placeholder="Your Email :"
+                        />
+                      </div>
+                      <div class="mb-4 ">
+                        <label for="exampleFormControlInput5" class="form-label">
+                          Address
+                        </label>
+                        <input
+                          type="text"
+                          class="form-control"
+                          id="exampleFormControlInput5"
+                          placeholder="Your Email :"
+                        />
+                      </div>
 
+                      <div className="mb-4">
+                        <label for="exampleFormControlInput3" class="form-label">
+                          Marital Status
+                        </label>
+                        <select class="form-select" aria-label="Default select example">
+                          <option selected>Select </option>
+                          <option value="Single">Single</option>
+                          <option value="Married">Married</option>
+                        </select>
+                      </div>
+                    </div>
                   </div>
-                  <div className="w-50 ms-3">
-                  <div class="mb-3 ">
-                    <label for="exampleFormControlInput1" class="form-label">
-                    Your Email 
-                    </label>
-                    <input
-                      type="email"
-                      class="form-control"
-                      id="exampleFormControlInput1"
-                      placeholder="Your Email :"
-                    />
-                  </div>
-                  <div class="mb-3  ">
-                    <label for="exampleFormControlInput1" class="form-label">
-                    Your Email 
-                    </label>
-                    <input
-                      type="email"
-                      class="form-control"
-                      id="exampleFormControlInput1"
-                      placeholder="Your Email :"
-                    />
-                  </div>
-                  <div class="mb-3 ">
-                    <label for="exampleFormControlInput1" class="form-label">
-                    Your Email 
-                    </label>
-                    <input
-                      type="email"
-                      class="form-control"
-                      id="exampleFormControlInput1"
-                      placeholder="Your Email :"
-                    />
-                  </div>
-                  <div class="mb-3  ">
-                    <label for="exampleFormControlInput1" class="form-label">
-                    Your Email 
-                    </label>
-                    <input
-                      type="email"
-                      class="form-control"
-                      id="exampleFormControlInput1"
-                      placeholder="Your Email :"
-                    />
-                  </div>
-                  <div class="mb-3  ">
-                    <label for="exampleFormControlInput1" class="form-label">
-                    Your Email 
-                    </label>
-                    <input
-                      type="email"
-                      class="form-control"
-                      id="exampleFormControlInput1"
-                      placeholder="Your Email :"
-                    />
-                  </div>
+                  {/* others info */}
+                  <h5 className="my-5 fw-bold">Others Info :</h5>
+                  <div className="d-flex">
+                    <div className="from-three w-50">
+                      <div class="mb-4 ">
+                        <label for="exampleFormControlInput5" class="form-label">
+                          Address
+                        </label>
+                        <input
+                          type="text"
+                          class="form-control"
+                          id="exampleFormControlInput5"
+                          placeholder="Your Email :"
+                        />
+                      </div>
+                      <div class="mb-4 ">
+                        <label for="exampleFormControlInput2" class="form-label">
+                          National ID
+                        </label>
+                        <input
+                          type="number"
+                          class="form-control"
+                          id="exampleFormControlInput2"
+                          placeholder="Your Email :"
+                        />
+                      </div>
+                      <div className="mb-4">
+                        <label for="exampleFormControlInput3" class="form-label">
+                          Gender
+                        </label>
+                        <select class="form-select" aria-label="Default select example">
+                          <option selected>Select Gender</option>
+                          <option value="Male">Male</option>
+                          <option value="Female">Female</option>
+                        </select>
+                      </div>
+                    </div>
+                    <div className="from-four w-50 ms-4">
+                      <div class="mb-4 ">
+                        <label for="exampleFormControlInput4" class="form-label">
+                          Passport Number
+                        </label>
+                        <input
+                          type="number"
+                          class="form-control"
+                          id="exampleFormControlInput4"
+                          placeholder="Your Email :"
+                        />
+                      </div>
 
+                      <div className="mb-4">
+                        <label for="exampleFormControlInput3" class="form-label">
+                          Marital Status
+                        </label>
+                        <select class="form-select" aria-label="Default select example">
+                          <option selected>Select </option>
+                          <option value="Single">Single</option>
+                          <option value="Married">Married</option>
+                        </select>
+                      </div>
+                    </div>
                   </div>
-                </div>
-                 
                 </div>
               </div>
             </div>
