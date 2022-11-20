ebb52b3f6013a8eff2e42e017ec31e6b87f94f17
RHsanto <rakibulsanto789@gmail.com>
2022-11-20 22:33:02 +0600

    add dashboard

--- src/components/DashBoard/UserProfile/UserInfo.js
+++ src/components/DashBoard/UserProfile/UserInfo.js
@@ -2,14 +2,13 @@
 import React from "react";
 import useFirebase from "../../../hooks/useFirebase";
 
-
 const UserInfo = () => {
-  const {user}=useFirebase();
-  
+  const { user } = useFirebase();
+
   return (
     <div className="user-info-section">
       <div className="card">
-         <h4 className=" profile-title text-center">Personal Info </h4>
+        <h4 className=" profile-title text-center">Personal Info </h4>
         <div className="card-item p-4 mt-5">
           <div className="all-item ps-3">
             <div className="personal-item ">
@@ -19,11 +18,11 @@ const UserInfo = () => {
               <p>Phone </p>
               <p>Gender </p>
               <p>Marital Status </p>
-              <p>National ID </p>   
+              <p>National ID </p>
             </div>
 
-          <div className="personal-item-result fw-bold ms-5 w-75">
-             <p className="item-result">{user?.displayName}</p>
+            <div className="personal-item-result fw-bold ms-5 w-75">
+              <p className="item-result">{user?.displayName}</p>
               <p className="item-result">{user?.email}</p>
               <p className="item-result">N/A</p>
               <p className="item-result">N/A</p>
@@ -31,8 +30,6 @@ const UserInfo = () => {
               <p className="item-result">N/A</p>
               <p className="item-result">N/A</p>
             </div>
-          
-            
           </div>
         </div>
       </div>
