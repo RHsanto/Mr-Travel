19113034f4a5e76f542ad19ef22b4fc083a08dd1
RHsanto <rakibulsanto789@gmail.com>
2022-11-20 19:50:48 +0600

    add dashboard

--- src/components/DashBoard/UserProfile/UserInfo.js
+++ src/components/DashBoard/UserProfile/UserInfo.js
@@ -1,9 +1,23 @@
-import React from 'react';
+import React from "react";
 
 const UserInfo = () => {
   return (
-    <div>
-      <h1>UserInfo</h1>
+    <div className="user-info-section">
+      <div className="card">
+        <div className="card-item p-4">
+          <h5 className="mb-5">Personal Info</h5>
+          <div className="all-item">
+            <div className="mb-4 personal-item d-flex  border-bottom border-1 ">
+              <p>Name :</p>
+              <p className="ms-5">Rakibul Hasan Santo</p>
+            </div>
+            <div className="mb-4 personal-item d-flex  border-bottom border-1 ">
+              <p>Name :</p>
+              <p className="ms-5">Rakibul Hasan Santo</p>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
