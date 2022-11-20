d9844ae3ac1dfce39a3c05f472f639e5cca26382
RHsanto <rakibulsanto789@gmail.com>
2022-11-20 20:38:09 +0600

    add dashboard

--- src/components/DashBoard/UserProfile/UserInfo.js
+++ src/components/DashBoard/UserProfile/UserInfo.js
@@ -9,38 +9,27 @@ const UserInfo = () => {
         <div className="card-item p-4">
           <h5 className="mb-5">Personal Info</h5>
           <div className="all-item ps-3">
-            <div className="mb-4 personal-item d-flex  border-bottom border-1 ">
-              <p className="text-secondary">Name </p>
-              <p className="ms-5 fw-bold">{user?.displayName}</p>
+            <div className="personal-item ">
+              <p>Name </p>
+              <p>Email </p>
+              <p>Address </p>
+              <p>Phone </p>
+              <p>Gender </p>
+              <p>Marital Status </p>
+              <p>National ID </p>   
             </div>
-            <div className="mb-4 personal-item d-flex  border-bottom border-1 ">
-              <p className="text-secondary">Email </p>
-              <p className="ms-5 fw-bold">{user?.email}</p>
+
+          <div className="personal-item-result ms-5 fw-bold">
+             <p className="item-result">{user?.displayName}</p>
+              <p className="item-result">{user?.email}</p>
+              <p className="item-result">N/A</p>
+              <p className="item-result">N/A</p>
+              <p className="item-result">N/A</p>
+              <p className="item-result">N/A</p>
+              <p className="item-result">N/A</p>
             </div>
-            <div className="mb-4 personal-item d-flex  border-bottom border-1 ">
-              <p className="text-secondary">Address </p>
-              <p className="ms-5 fw-bold">N/A</p>
-            </div>
-            <div className="mb-4 personal-item d-flex  border-bottom border-1 ">
-              <p className="text-secondary">Phone </p>
-              <p className="ms-5 fw-bold">N/A</p>
-            </div>
-            <div className="mb-4 personal-item d-flex  border-bottom border-1 ">
-              <p className="text-secondary">Gender </p>
-              <p className="ms-5 fw-bold">N/A</p>
-            </div>
-            <div className="mb-4 personal-item d-flex  border-bottom border-1 ">
-              <p className="text-secondary">Marital Status </p>
-              <p className="ms-5 fw-bold">N/A</p>
-            </div>
-            <div className="mb-4 personal-item d-flex  border-bottom border-1 ">
-              <p className="text-secondary">National ID </p>
-              <p className="ms-5 fw-bold">N/A</p>
-            </div>
-            <div className="mb-4 personal-item d-flex  border-bottom border-1 ">
-              <p className="text-secondary">Passport Number	 </p>
-              <p className="ms-5 fw-bold">N/A</p>
-            </div>
+          
+            
           </div>
         </div>
       </div>
