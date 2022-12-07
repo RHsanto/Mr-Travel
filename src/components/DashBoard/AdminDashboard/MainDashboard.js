ae25a4ae27c6a7f5608df227345663510d668762
RHsanto <rakibulsanto789@gmail.com>
2022-12-07 14:59:10 +0600

    start admin dash make

--- src/components/DashBoard/AdminDashboard/MainDashboard.js
+++ src/components/DashBoard/AdminDashboard/MainDashboard.js
@@ -1,10 +1,21 @@
 import React from 'react';
+import { FcHome } from 'react-icons/fc';
+import { Link } from 'react-router-dom';
 
 const MainDashboard = () => {
   return (
     <div>
-      <h1>MainDashboard
-MainDashboard</h1>
+      <div className="">
+        <div className=" dashboard-header">
+          <div className=" d-flex justify-content-between align-items-center">
+           <Link to='/'>
+            <h5 className='d-flex align-items-center text-dark'>
+             <FcHome className='me-1'/> Home</h5>
+          </Link>
+           <div><h3>Profile</h3></div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
