8e8c9d78ea09f721b04d31a415c27e379b675726
RHsanto <rakibulsanto789@gmail.com>
2022-12-07 23:54:49 +0600

    add booked table

--- src/components/DashBoard/AdminDashboard/MainDashboard.js
+++ src/components/DashBoard/AdminDashboard/MainDashboard.js
@@ -74,8 +74,8 @@ const MainDashboard = () => {
         </div>
         {/* here booking and payment table */}
         <div className="container-fluid px-1 mt-5">
-          <div className="row">
-            <div className="col-lg-6">
+          <div className="row ">
+            <div className="col">
               <div className="booking-table shadow rounded">
                 <h4 className="table-title ps-4">Recent Booking</h4>
                 <hr />
@@ -93,19 +93,56 @@ const MainDashboard = () => {
                     {booking.map(data => (
                       <tr>
                         <th scope="row">{data?.firstName}</th>
-                        <td>Mark</td>
-                        <td>Otto</td>
-                        <td>@mdo</td>
-                        <td>@mdo</td>
+                        <td>{data?.bookingDate ? data?.bookingDate : "8/2/2022"}</td>
+                        <td>{data?.types}</td>
+                        <td>
+                          <button className="border-0  bg-warning p-1 rounded">
+                            <small>pending</small>
+                          </button>
+                          <button className="border-0 fs-6 bg-success text-light ms-3 p-1 rounded">
+                            <small>Approved</small>
+                          </button>
+                        </td>
+                        <td>${data?.price}</td>
                       </tr>
                     ))}
                   </tbody>
                 </table>
               </div>
             </div>
-            <div className="col-lg-6">
-              <div className="payment-table">
-                <h2>hviohioph</h2>
+            <div className="col mt-5">
+              <div className="booking-table shadow rounded">
+                <h4 className="table-title ps-4">Payments</h4>
+                <hr />
+                <table className="table ">
+                  <thead>
+                    <tr>
+                      <th scope="col">Date</th>
+                      <th scope="col">Clients</th>
+                      <th scope="col">Service</th>
+                      <th scope="col">Status</th>
+                      <th scope="col">Price</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    {booking.map(data => (
+                      <tr>
+                        <td>{data?.bookingDate ? data?.bookingDate : "8/2/2022"}</td>
+                        <th scope="row">{data?.firstName}</th>
+                        <td>{data?.types}</td>
+                        <td>${data?.price}</td>
+                        <td>
+                          <button className="border-0  bg-warning p-1 rounded">
+                            <small>pending</small>
+                          </button>
+                          <button className="border-0 fs-6 bg-success text-light ms-3 p-1 rounded">
+                            <small>Approved</small>
+                          </button>
+                        </td>
+                      </tr>
+                    ))}
+                  </tbody>
+                </table>
               </div>
             </div>
           </div>
