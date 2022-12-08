ba0cc1ec9109bab86f8f82307d2ac40556098314
RHsanto <rakibulsanto789@gmail.com>
2022-12-09 00:06:52 +0600

    add approved and delete

--- src/components/DashBoard/AdminDashboard/Services.js
+++ src/components/DashBoard/AdminDashboard/Services.js
@@ -1,27 +1,29 @@
-import React from 'react';
-import useSWR from 'swr';
-import AdminDashHeader from './AdminDashHeader';
+import React from "react";
+import useSWR from "swr";
+import AdminDashHeader from "./AdminDashHeader";
 import { MdGridView } from "react-icons/md";
 
 // useSWR data fetcher
 const fetcher = (...args) => fetch(...args).then(res => res.json());
 
 const Services = () => {
-
-  const { data: allTravelsData } = useSWR(`https://mr-travel-server.onrender.com/allTravelsData`, fetcher);
+  const { data: allTravelsData } = useSWR(
+    `https://mr-travel-server.onrender.com/allTravelsData`,
+    fetcher
+  );
 
   return (
     <div>
-      <AdminDashHeader/>
+      <AdminDashHeader />
       <div className="service-info-section p-3">
-      <h3>Services</h3>
-         {/* here booking and payment table */}
-         <div className="container-fluid px-1 mt-5">
+        <h3>Here All Services</h3>
+        {/* here booking and payment table */}
+        <div className="container-fluid px-1 mt-5">
           <div className="row ">
             <div className="col">
               <div className="booking-table shadow rounded">
                 <h4 className="table-title ps-4">Recent Booking</h4>
-                <hr  />
+                <hr />
                 <table className="table ">
                   <thead>
                     <tr>
@@ -36,25 +38,29 @@ const Services = () => {
                   <tbody>
                     {allTravelsData?.map(data => (
                       <tr>
-  
-                        <td>{data?.hotelName || data?.TourName || data?.types  } </td>
+                        <td>
+                          <img className="service-img me-2" src={data?.img} alt="img" />
+                          {data?.hotelName || data?.TourName || data?.types}{" "}
+                        </td>
                         <td>{data?.location || "Dhaka"}</td>
                         <td>{data?.types}</td>
                         <td>${data?.price}</td>
                         <td>{data?.types}</td>
-                        <td > <div className='view-btn'>
-                        <MdGridView/> View
-                          </div> </td>
-              
+                        <td>
+                          {" "}
+                          <div className="view-btn">
+                            <MdGridView /> View
+                          </div>{" "}
+                        </td>
                       </tr>
                     ))}
                   </tbody>
                 </table>
               </div>
             </div>
-            </div>
+          </div>
+        </div>
       </div>
-    </div>
     </div>
   );
 };
